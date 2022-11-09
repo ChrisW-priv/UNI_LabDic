dcd630249b21c421e3e0f311d95f49dcf33afdef
ChrisW-priv <chris.watras@gmail.com>
2022-11-09 15:42:34 +0100

    tests.cpp is being added, (much work needed)

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,33 @@
 #include "dictionary.h"
 
+// return codes tor tests:
+// 1 - success
+// 0 - fail
 
-int test_1(){
-    int x = 1;
-    return 0;
+template<typename T>
+bool compare_with_expected(T value_expected, T value_returned){
+    return value_expected == value_returned;
+}
+
+bool test_1(){
+    dictionary<int, int> dic{};
+    bool ret = dic.empty();
+
+    return compare_with_expected(true, ret);
 };
+
+
+bool test_2(){
+    dictionary<int, int> dic{};
+    int ret = dic.size();
+
+    return compare_with_expected(0, ret);
+};
+
+
+bool test_3(){
+    dictionary<int, int> dic{};
+    dic.emplace(1, 1);
+    dic.emplace(1, 2);
+
+}
